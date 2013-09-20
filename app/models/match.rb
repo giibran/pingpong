558e434e6b752f348704beb9192de0cc8041226a
T-1000 <t-1000@t-1000.(none)>
2013-09-19 23:01:33 -0500

    working with models

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,3 +1,10 @@
 class Match < ActiveRecord::Base
-  has_many :players
+	has_one :player1, :class_name => 'Player', :foreign_key => 'id_player1'
+  has_one :player2, :class_name => 'Player', :foreign_key => 'id_player2'
+  validates :id_player1, presence: true
+  validates :score_player1, presence: true
+  validates :id_player2, presence: true
+  validates :score_player2, presence: true
+  validates :score_player1, numericality: { only_integer: true }
+  validates :score_player2, numericality: { only_integer: true }
 end
